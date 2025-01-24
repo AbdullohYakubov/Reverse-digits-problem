ebcaa09cceaba4845a047c95ccc0cf62fd101436
Abdullokh Yokubov <abdullokhyokubov@Abdullokhs-MacBook-Air.local>
2025-01-24 12:23:14 +0400

    solved reversing digits problem, makeing it ignore 0 when reversing. Might optimize the code to minimize time and space complexity

---
+++ Problem1.java
@@ -0,0 +1,25 @@
+// Problem Statement: Given a non-negative integer u as input, reverse the digits of
+// u and return the result. For example, if u = 123, the output should be 321.
+
+import java.util.Scanner;
+
+public class Problem1{
+    public static void main(String[] args){
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int num = scr.nextInt();
+        int rev = 0;
+
+        while(num != 0){
+            if(num % 10 != 0){
+                rev = rev * 10 + num % 10;
+            }else{
+                rev = rev + num % 10;
+            }
+
+            num = num / 10;
+        }
+
+        System.out.println("The reversed number is: " + rev);
+    }    
+}
